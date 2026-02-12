571f563b266c76debb254c28b29d9d3e4df1e553
HornetSys <peng.kang@csus.edu>
2026-02-12 15:18:19 -0800

    Update events.h

--- 2.Keyboard_Interrupt/include/events.h
+++ 2.Keyboard_Interrupt/include/events.h
@@ -5,7 +5,7 @@
 #ifndef __EVENTS_H__
 #define __EVENTS_H__
 
-#define KEYBOARD_EVENT 33 // 32-bit long
+#define KEYBOARD_EVENT 0x21 // decimal: 33
 
 #ifndef ASSEMBLER  // skip if ASSEMBLER defined (in assembly code)
 void KeyboardEntry(); // defined in events.S
